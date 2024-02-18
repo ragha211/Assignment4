2f7afcdb433b23f35eeaeb79d91804aeef4f1929
ragha211 <151955497+ragha211@users.noreply.github.com>
2024-02-18 18:43:26 +0300

    Add files via upload

---
+++ Q2.java
@@ -0,0 +1,16 @@
+//يقوم بطباعه العنصر الثاني من نهايه ال NODE وعنوان الذاكره الخاص ب i
+
+public class Q2 {
+    public static void main(String[] args) {
+        singleLinkedList<Integer>list=new singleLinkedList<>();
+
+        list.addFirst(11);
+        list.addLast(12);
+        list.addLast(13);
+        list.addLast(14);
+        list.addLast(15);
+        singleLinkedList.Node<Integer>i= list.getSecondLast();
+        System.out.println(i.getElement());
+        System.out.println(i);
+    }
+}
